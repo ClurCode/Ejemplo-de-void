76f8c528679544d155b10108e366b259c9718863
ClurCode <38564440+ClurCode@users.noreply.github.com>
2018-06-20 20:12:48 -0300

    Update Ejemplos de void.cpp

--- Ejemplos de void.cpp
+++ Ejemplos de void.cpp
@@ -10,18 +10,18 @@ int main(int argc, char const *argv[])
 	int resultado, valor1 = 3, valor2 = 5;
 	//Llamada a funcion
 	no_retorno_suma(valor1, valor2, resultado); //Ambas funciones llegaran al mismo valor (3+5=8)
-												//Pero en pantalla no se verá nada, porque las funciones 
-	retorna_entero_suma(valor1, valor2);		//no tienen un cout
+						    //Pero en pantalla no se verá nada, porque las funciones 
+	retorna_entero_suma(valor1, valor2);	    //no tienen un cout
 	//Para imprimir el resultado de la funcion void
 	// std::cout<<no_retorno_suma(valor1, valor2, resultado); da error porque no tiene ningun
 	//valor, lo que se debe hacer es:
 	std::cout<<resultado; // esta variable resultado, fue afectada por la funcion no_retorno_suma
-						  // por eso el "&", asi que ahora resultado tiene el valor de valor1+valor2
+			      // por eso el "&", asi que ahora resultado tiene el valor de valor1+valor2
 	// Para la funcion con retorno, si se puede mostrar directamente, y no quedará guardado
 	std::cout<<retorna_entero_suma(valor1, valor2); // En pantalla se verá 8
 	// Para funciones con retorno tambien se ve valido
 	int a = retorna_entero_suma(valor1, valor2);// Si y solo si, variable a, es del mismo
-												// tipo que funcion f, en este caso int
+			                            // tipo que funcion f, en este caso int
 	// Tambien son validas todas las operaciones (suma, resta, multiplicacion, division, etc)
 	a = retorna_entero_suma(valor1, valor2) + resultado; 
 	std::cout<<a; // Esto mostrará en pantalla 16
